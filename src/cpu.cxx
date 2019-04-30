9c6abf9a259099498fa9c91aeff173c25f5c0761
mkydh <michael.kevin@gmail.com>
2019-04-29 23:25:35 -0500

    updated some cpu methods and added clock count

--- src/cpu.cxx
+++ src/cpu.cxx
@@ -3,15 +3,17 @@
 CPU::CPU(std::shared_ptr<Mem> memory) {
     this->memory = memory;
     
+    reg_s = 0xFF;
     reg_pc = memory->reset_vector();
 }
 
 void CPU::execute() {
-    uint16_t opcode = memory->mem_read(reg_pc);
+    cycles = 0;
+    uint8_t opcode = pc_read();
     
     switch (opcode) {
         case LDA_I: {
-            uint16_t operand = memory->mem_read(reg_pc + 1);
+            uint16_t operand = pc_read();
             
             reg_ac = (uint8_t) operand;
             break;
@@ -24,131 +26,99 @@ void CPU::execute() {
             set_interrupt(1);
             break;
         }
-
         case BCC: {
-	    uint16_t operand = memory->mem_read(reg_pc + 1);
+            uint8_t operand = pc_read();
             if (!get_carry()){
-		reg_pc += operand;
-	    }
-
-	    reg_pc += 2;
-	    break;
-	}
-
-        case BCS: {
-	    uint16_t operand = memory->mem_read(reg_pc + 1);
-	    if (get_carry()){
-		reg_pc += operand;
-	    }
-
-	    reg_pc += 2;
-	    break;
-
-	}
-
-	case BEQ: {
-	    uint16_t operand = memory->mem_read(reg_pc + 1);
-	    if (get_zero()) {
-		reg_pc += operand;
-	    }	
-	    reg_pc += 2;
-	    break;
-	}
-
-        case BNE: {
-	    uint16_t operand = memory->mem_read(reg_pc + 1);
-	    if (!get_zero()) {
-		reg_pc += operand;
-	    }
-
-	    reg_pc += 2;
-	    break;
-
-	}
-
-        case BVC: {
-	    uint16_t operand = memory->mem_read(reg_pc + 1);
-	    if (!get_overflow()) {
-		reg_pc += operand;
-	    }
-	    reg_pc += 2;
-	    break;
-	}
-
-        case BVS: {
-	    uint16_t operand = memory->mem_read(reg_pc + 1);
-	    if (get_overflow()) {
-		reg_pc += operand;
-	    }
-
-	    reg_pc += 2;
-	    break;
+                reg_pc += operand;
+            }
+            break;
         }
-
+        case BCS: {
+            uint8_t operand = pc_read();
+            if (get_carry()){
+                reg_pc += operand;
+            }
+            break;
+        }
+        case BEQ: {
+            uint8_t operand = pc_read();
+            if (get_zero()) {
+                reg_pc += operand;
+            }	
+            break;
+        }
+        case BNE: {
+            uint8_t operand = pc_read();
+            if (!get_zero()) {
+                reg_pc += operand;
+            }
+            break;
+    
+        }
+        case BVC: {
+            uint8_t operand = pc_read();
+            if (!get_overflow()) {
+                reg_pc += operand;
+            }
+            break;
+        }
+        case BVS: {
+            uint8_t operand = pc_read();
+            if (get_overflow()) {
+                reg_pc += operand;
+            }
+            reg_pc += 2;
+            break;
+        }
         case BPL: {
-	    uint16_t operand = memory->mem_read(reg_pc + 1);
-	    if (!get_negative()) {
-		reg_pc += operand;
-	    }
-	    
-	    reg_pc += 2;
-	    break;
-	}
-
+            uint8_t operand = pc_read();
+            if (!get_negative()) {
+                reg_pc += operand;
+            }
+            break;
+        }
         case BMI: {
-	    uint16_t operand = memory->mem_read(reg_pc + 1);
-	    if (get_negative()) {
-		reg_pc += operand;
-	    }
-
-	    reg_pc += 2;
-	    break;
-	}
-
+            uint8_t operand = pc_read();
+            if (get_negative()) {
+                reg_pc += operand;
+            }
+            break;
+        }
         case JMP_I: {
-            uint16_t operand = memory->mem_read(reg_pc + 1) + (memory->mem_read(reg_pc + 2) << 8);
-	    uint16_t nextMem = ((operand + 1) % 0xFF) + (operand >> 8);
-
-	    reg_pc = memory->mem_read(operand) + (memory->mem_read(nextMem) << 8);
-	
-	    break;
-	}
-
-	case JMP_A: {
-	    reg_pc = memory->mem_read(reg_pc + 1) + (memory->mem_read(reg_pc + 2) << 8);
-	    break;
-	}
-	
+            uint16_t operand = pc_read();
+            uint16_t nextMem = ((operand + 1) % 0xFF) + (operand >> 8);
+    
+            reg_pc = mem_read(operand) + (mem_read(nextMem) << 8);
+            break;
+        }
+        case JMP_A: {
+            reg_pc = pc_read2();
+            break;
+        }
         case JSR: {
-	    uint16_t operand = memory->mem_read(reg_pc + 1) + (memory->mem_read(reg_pc + 2) << 8) - 1;
-	    memory->mem_write(reg_s - 1, operand >> 8);
-	    memory->mem_write(reg_s - 2, operand % 0xFF);
-	    reg_s -= 2;
-	    reg_pc = operand + 1;	    
-  
-	    break;
-	}
-
+            uint16_t operand = pc_read2() - 1;
+            push(operand >> 8);
+            push(operand & 0xFF);
+            reg_pc = operand + 1;	    
+            break;
+        }
         case RTI: {
-	    reg_p = memory->mem_read(reg_s) + (memory->mem_read(reg_s - 1) << 8);
-	    reg_pc = memory->mem_read(reg_s - 2) + (memory->mem_read(reg_s - 3) << 8);
-	    reg_s += 4;    
-
-	    break;
-	}
-
-	case RTS: {
-            uint16_t operand = memory->mem_read(reg_s) + (memory->mem_read(reg_s + 1) << 8);
-	    reg_s += 2;
-	    reg_pc = operand + 1;
-
-	    break;
-	}
-
-	
-        
+            reg_p = pop();
+            reg_pc = pop16();
+            break;
+        }
+        case RTS: {
+            reg_pc = pop16() + 1;
+            break;
+        }
+        default: {
+            break;
+        }
         // add all the other opcodes
     }
+    
+    std::cout << "opcode: " << std::hex << unsigned(opcode) << std::endl;
+    std::cout << "cycles: " << unsigned(cycles) << std::endl;
 }
 
 void CPU::set_negative(bool value) {
@@ -184,6 +154,55 @@ void CPU::set_zero(bool value) {
 void CPU::set_carry(bool value) {
     reg_p &= 0xfe;
     reg_p |= value;
+}
+
+uint8_t CPU::mem_read(uint64_t index) {
+    cycles++;
+    return memory->mem_read(index);
+}
+
+uint16_t CPU::mem_read2(uint64_t index) {
+    cycles += 2;
+    return memory->mem_read2(index);
+}
+
+void CPU::mem_write(uint64_t index, uint8_t value) {
+    cycles++;
+    memory->mem_write(index, value);
+}
+
+uint8_t CPU::pc_read() {
+    uint8_t data = mem_read(reg_pc);
+    reg_pc++;
+    return data;
+}
+
+uint16_t CPU::pc_read2() {
+    uint16_t data = mem_read2(reg_pc);
+    reg_pc += 2;
+    return data;
+}
+
+void CPU::push(uint8_t value) {
+    memory->mem_write(((uint16_t) reg_s) + 0x100, value);
+    reg_s--;
+}
+
+void CPU::push16(uint16_t value) {
+    push((uint8_t) value);
+    push((uint8_t) (value >> 8));
+}
+
+uint8_t CPU::pop() {
+    uint8_t data = (uint8_t) memory->mem_read(((uint16_t) reg_s) + 0x100);
+    reg_s++;
+    return data;
+}
+
+uint16_t CPU::pop16() {
+    uint16_t data = ((uint16_t) pop()) << 8;
+    data += pop();
+    return data;
 }
 
 bool CPU::get_negative() {
