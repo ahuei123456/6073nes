2ed46055df189932630a814ce53f32b1f8862d7b
mkydh <michael.kevin@gmail.com>
2019-05-02 02:19:16 -0500

    ppu skeleton

--- src/mem.hpp
+++ src/mem.hpp
@@ -10,29 +10,41 @@
 #include "ppu.hpp"
 #include "cpu.hpp"
 
-#define NROM_128 16384
-#define NROM_256 32768
-#define NROM_START 0x8000
+#define NROM_128        16384
+#define NROM_256        32768
+#define NROM_START      0x8000
 
-#define CPU_MEM_SIZE 0x10000
-#define PPU_MEM_SIZE 0x3FFF
+#define CPU_MEM_SIZE    0x10000
+#define PPU_MEM_SIZE    0x3FFF
 
-#define RESET_VECTOR 0xFFFC
+#define VALID_CPU_INDEX(index) (index < CPU_MEM_SIZE)
+
+#define RAM             0x800
+#define VALID_RAM_INDEX(index) (index >= 0 && index < PPU_START)
+#define ACTUAL_RAM_ADDRESS(index) (index % RAM)
+
+#define RESET_VECTOR    0xFFFC
+#define VALID_ROM_INDEX(index) (index >= NROM_START && index < CPU_MEM_SIZE)
+#define ACTUAL_ROM_ADDRESS(index) (index - NROM_START)
 
 // ppu stuff
 
-#define PPUCTRL     0x2000
-#define PPUMASK     0x2001
-#define PPUSTATUS   0x2002
-#define OAMADDR     0x2003
-#define OAMDATA     0x2004
-#define PPUSCROLL   0x2005
-#define PPUADDR     0x2006
-#define PPUDATA     0x2007
-#define OAMDMA      0x4014
+#define PATTERN_TABLE   0x1000
+#define NAMETABLE       0x400
+
+#define PPU_START       0x2000
+#define PPUCTRL         0x2000
+#define PPUMASK         0x2001
+#define PPUSTATUS       0x2002
+#define OAMADDR         0x2003
+#define OAMDATA         0x2004
+#define PPUSCROLL       0x2005
+#define PPUADDR         0x2006
+#define PPUDATA         0x2007
+#define OAMDMA          0x4014
 
 #define VALID_PPU_INDEX(index) (index >= 0x2000 && index <= 0x3FFF)
-#define ACTUAL_ADDRESS(index) ((index - 0x2000) % 8 + 0x2000)
+#define ACTUAL_PPU_REGISTER(index) ((index - 0x2000) % 8 + 0x2000)
 #define PPU_REGISTER_WRITABLE(index) (!(index == 0x2002))
 #define PPU_REGISTER_READABLE(index) (index == 0x2002 || index == 0x2004 || index == 0x2007)
 
@@ -42,6 +54,15 @@ class Mem {
 private: 
     std::array<uint8_t, CPU_MEM_SIZE> cpu_mem;
     std::array<uint8_t, PPU_MEM_SIZE> ppu_mem;
+    
+    // cpu
+    std::array<uint8_t, RAM> ram;
+    std::array<uint8_t, CPU_MEM_SIZE - NROM_START> prg_rom;
+    
+    // ppu
+    std::array<uint8_t, PATTERN_TABLE> left;
+    std::array<uint8_t, PATTERN_TABLE> right;
+    std::array<std::array<uint8_t, NAMETABLE>, 4> nametables;
     
     // ppu stuff accessible by cpu
     uint8_t ppu_reg_read(uint64_t index);
@@ -56,11 +77,11 @@ public:
     uint16_t reset_vector();
     uint8_t mem_read(uint64_t index);
     uint16_t mem_read2(uint64_t index);
-    uint32_t mem_read3(uint64_t index);
-    uint32_t mem_read4(uint64_t index);
     void mem_write(uint64_t index, uint8_t value);
     
     // ppu only methods
+    uint8_t ppu_read(uint64_t index);
+    uint8_t ppu_write(uint64_t index, uint8_t value);
 };
 
 #endif
---
+++ src/ppu.hpp
@@ -0,0 +1,48 @@
+#ifndef ppu_hpp
+#define ppu_hpp
+
+#include <cstdint>
+#include <array>
+#include <exception>
+#include <memory>
+#include <SDL.h>
+#include "mem.hpp"
+
+#define SPRITES 0x40
+#define SPRITES_SEC 8
+#define LEFT 0xFFF
+#define RIGHT 0xFFF
+#define REGS 8
+
+class Mem;
+
+struct Sprite{
+    uint8_t Y;
+    uint8_t index;
+    uint8_t attributes;
+    uint8_t X;
+    
+    uint8_t palette();
+    uint8_t priority();
+    uint8_t horizontal_flip();
+    uint8_t vertical_flip();
+};
+
+class PPU {
+private:
+    std::shared_ptr<Mem> memory;
+
+    std::array<uint8_t, REGS> regs;
+    std::array<Sprite, SPRITES> oam;
+    std::array<Sprite, SPRITES_SEC> oam_sec;
+    
+    uint16_t vram_addr;
+
+public:
+    PPU(std::shared_ptr<Mem> memory);
+    void set_reg(uint64_t index, uint8_t value);
+    
+    void set_oam();
+};
+
+#endif
